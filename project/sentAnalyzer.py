418f6002cd18871ea8360594d0625cefdf98cabf
rizaluturk <riza.yarar@gmail.com>
2020-05-19 21:04:35 +0200

    implementation of filtering
    implementation of filtering for question page
    filterhistory.json contain the gener of films and answer for question
    movie.json now contain also genre and years 
    moviesent.json now contain also genre
    filtermovie.psìy is script for filtering movie from moviesent.json
    sentAnalyzer.py is implemented with genre extraction from movie.json

--- project/sentAnalyzer.py
+++ project/sentAnalyzer.py
@@ -1,16 +1,26 @@
-
 import json
 from analyzer import analyzer
+
+
 def main():
     movie2 = []
+    genres = []
     with open('movie.json') as infile:
         movie = json.load(infile)
-    # print(movie[0]['overview'])
-    with open('moviesent2.json', 'w') as f:
+
+    with open('moviesent.json', 'w') as f:
         for i in range(0, 1000):
-            movie2.append({"title": movie[i]['title'], "ratings": movie[i]['vote_average'], "sentiment": analyzer(movie[i]['overview'])})
+            movie2.append({"title": movie[i]['title'], "ratings": movie[i]['vote_average'],"genres":movie[i]['genres'],"sentiment": analyzer(movie[i]['overview'])})
         json.dump(movie2, f, indent=2)
 
+    with open('genres.json', 'w') as f1:
+        for i in range(0, len(movie)):
+            txt = movie[i]['genres']
+            x = txt.split("|")
+            for y in range(0, len(x)):
+                if x[y] not in genres:
+                    genres.append(x[y])
+        json.dump(genres, f1, indent=2)
 
 
 if __name__ == "__main__":
