75668e0b6eeff304aecc9d4fda5f2910bca5de51
paulinajewulska <paulina.jewulska@gmail.com>
2020-05-21 18:10:45 +0200

    Fix some bugs

--- project/analyzer.py
+++ project/analyzer.py
@@ -4,14 +4,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 
-#nltk.download('stopwords')
-#set(stopwords.words('english'))
+nltk.download('stopwords')
 
 
 def analyzer(text):
     stop_words = stopwords.words('english')
     text1 = text
-    processed_doc1 = ' '.join([word for word in text1.split() if word not in stop_words])
+    processed_doc1 = ' '.join(
+        [word for word in text1.split() if word not in stop_words])
     sa = SentimentIntensityAnalyzer()
     dd = sa.polarity_scores(text=processed_doc1)
     compound = round((1 + dd['compound']) / 2, 2)
--- project/filtermovie.py
+++ project/filtermovie.py
@@ -1,5 +1,5 @@
 import json
-from analyzer import analyzer
+from .analyzer import analyzer
 
 
 def filtermovie(genre, answer1, answer2, answer3, answer4):
@@ -8,28 +8,30 @@ def filtermovie(genre, answer1, answer2, answer3, answer4):
     sentanswer3 = analyzer(answer3)
     sentanswer4 = analyzer(answer4)
     sentgenre = analyzer(genre)
-    sentuser = (sentanswer1 + sentanswer2 + sentanswer3 + sentanswer4 + sentgenre) / 5
+    sentuser = (sentanswer1 + sentanswer2 +
+                sentanswer3 + sentanswer4 + sentgenre) / 5
     movie2 = []
-    with open('moviesent.json') as infile:
+
+    with open("project/moviesent.json", "r") as infile:
         movie = json.load(infile)
 
     for i in range(0, len(movie)):
         txt = movie[i]['genres']
         x = txt.split("|")
         for y in range(0, len(x)):
-            if genre in x[y]:
-                if movie[i]['sentiment'] >= sentuser:
+            if genre.lower() in x[y].lower():
+                if movie[i]['sentiment'] > sentuser:
                     movie2.append(
                         {"title": movie[i]['title'], "ratings": movie[i]['ratings'], "genres": movie[i]['genres']})
 
-    jsonFile = open("filterhistory.json", "r")  # Open the JSON file for reading
+    # Open the JSON file for reading
+    jsonFile = open("project/filterhistory.json", "r")
     data = json.load(jsonFile)  # Read the JSON into the buffer
     jsonFile.close()  # Close the JSON file
     data.append((genre, answer1, answer2, answer3, answer4))
-    ## Save our changes to JSON file
-    jsonFile = open("filterhistory.json", "w+")
+    # Save our changes to JSON file
+    jsonFile = open("project/filterhistory.json", "w+")
     jsonFile.write(json.dumps(data, indent=2))
     jsonFile.close()
 
     return (movie2)
-
